2e586aec153e019c5502a17fec5f24238e266d39
limengmeng04 <limengmeng04@baidu.com>
2023-02-09 20:29:22 +0800

    add log

--- run_case.sh
+++ run_case.sh
@@ -143,7 +143,7 @@ else
 fi
 
 err=$(cat result/*|grep "failure message"|wc -l)
-if [ $? -ne 0 ];then
+if [ $err -ne 0 ];then
     echo "run failed, logs"
     cat xuperchain/testnet/node1/logs/nohup.out
     tail xuperchain/testnet/node1/logs/xchain.log.wf
